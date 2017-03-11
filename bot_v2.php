e4145b9dcec1f8ac24ee14d4c28d1b97ad88fddc
Ligament <ntrances@hotmail.com>
2017-03-11 12:56:50 +0700

    update

--- bot_v2.php
+++ bot_v2.php
@@ -2,13 +2,13 @@
 
 include ('line-bot.php');
 
-$channelSecret = '5a02ad4858081691fd64b0ddaad9cee8';
-$access_token  = 'KDibenaEkXLtV0XXGj0le1FNu2PdiFiUHAsUzsGa/5GsxfTbJGbnlJ6f7TkwS1NhNnscm3GWCYnEMcKXUSp2/MqrK05xHqAjW5dylGPH1PMUU8Tl5WlFuV1DYCvVZLPrO95mv/2XprHvrtI3o8FD7QdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'U7e89954991a92e09af34d7239b50386c';
+$access_token  = 'dC08L00nDwAwasGu2gNqqyAJ6RvWUVoZFx8msUwBiIxQrvbg7VKypVZAO5WHaMGFfg85FL9YVl7UsVzbdndaZpJGDSRycm3wvQibZcp9UuPwAFGFh5icr0cVdbY5/44VS6vVdv2A4uDolwgN3+Z9bgdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
-	
+
 if (!empty($bot->isEvents)) {
-		
+
 	if ($bot->message->text == 'เราคือใคร')
 	{
 		if ($bot->source->userId == 'U7e89954991a92e09af34d7239b50386c')
@@ -19,7 +19,7 @@ if (!empty($bot->isEvents)) {
 		{
 			$recvmsg = 'ไม่รู้ครับ';
 		}
-			
+
 		$bot->replyMessageNew($bot->replyToken, $recvmsg);
 	}
 	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
@@ -31,7 +31,7 @@ if (!empty($bot->isEvents)) {
 	}
 
 	// Failed
-	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+	echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody();
 	exit();
 
 }
