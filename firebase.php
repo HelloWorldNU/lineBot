1a9f9af5ba737b57dedacceb25e6572944fdc6ec
Ligament <ntrances@hotmail.com>
2017-03-11 16:05:05 +0700

    update

--- firebase.php
+++ firebase.php
@@ -3,20 +3,19 @@
 include ('vendor/autoload.php');
 
 $firebase = Firebase::fromServiceAccount(__DIR__.'/helloworld-48dff-firebase-adminsdk-zpze8-0bbee8d5ef.json');
-$database = $firebase->getDatabase();
+$tokenHandler = $firebase->getTokenHandler();
 
-$newPost = $database
-  ->getReference('blog/posts')
-  ->push([
-      'title' => 'Post title',
-      'body' => 'This should probably be longer.'
-  ]);
+$uid = 'a-uid';
+$claims = ['foo' => 'bar']; // optional
 
-$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
-$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+// Returns a Lcobucci\JWT\Token instance
+$customToken = $tokenHandler->createCustomToken($uid, $claims);
+echo $token; // "eyJ0eXAiOiJKV1..."
 
-$newPost->getChild('title')->set('Changed post title');
-$newPost->getValue(); // Fetches the data from the realtime database
-$newPost->remove();
+$idTokenString = 'eyJhbGciOiJSUzI1...';
+// Returns a Lcobucci\JWT\Token instance
+$idToken = $tokenHandler->verifyIdToken($idTokenString);
 
-//echo $name;
+$uid = $idToken->getClaim('sub');
+
+echo $uid; // 'a-uid'
