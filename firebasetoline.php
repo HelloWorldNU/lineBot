29765d7d21b87a0d275ae2afa4636d8f8c3ac797
Ligament <ntrances@hotmail.com>
2017-03-11 17:29:46 +0700

    Update firebasetoline.php

--- firebasetoline.php
+++ firebasetoline.php
@@ -6,20 +6,22 @@ $secret = 'aeogVO99vj4QgwyUcR6QXl4OaPEnPB9izi9d9PCa';
 $databaseUri = 'https://helloworld-48dff.firebaseio.com';
 
 $firebase = Firebase::fromDatabaseUriAndSecret($databaseUri, $secret);
-
+echo "5555";
 $database = $firebase->getDatabase();
+echo $database;
 
-$newPost = $database
-    ->getReference('blog/posts')
-    ->push([
-        'title' => 'Post title',
-        'body' => 'This should probably be longer.'
-    ]);
+// $newPost = $database
+//     ->getReference('blog/posts')
+//     ->push([
+//         'title' => 'Post title',
+//         'body' => 'This should probably be longer.'
+//     ]);
 
-$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
-$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+// $newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
 
-$newPost->getChild('title')->set('Changed post title');
-$newPost->getValue(); // Fetches the data from the realtime database
-$newPost->remove();
+// $newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+
+// $newPost->getChild('title')->set('Changed post title');
+// $newPost->getValue(); // Fetches the data from the realtime database
+// $newPost->remove();
 ?>
