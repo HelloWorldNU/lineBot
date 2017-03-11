1cc498befdcf8f56d0962afaaceda9cf978126de
Ligament <ntrances@hotmail.com>
2017-03-11 13:48:40 +0700

    firebase

--- firebase.php
+++ firebase.php
@@ -1,21 +1,24 @@
 <?php
-const DEFAULT_URL = 'https://helloworld-48dff.firebaseio.com';
-const DEFAULT_TOKEN = 'AIzaSyAuz1ICCDhvX70gjOJPoPo0fMZfiav2fqc';
-const DEFAULT_PATH = '/firebase/line';
 
-$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+include ('vendor/autoload.php');
 
-// --- storing an array ---
-$test = array(
-    "foo" => "bar",
-    "i_love" => "lamp",
-    "id" => 42
-);
-$dateTime = new DateTime();
-$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+  const DEFAULT_URL = 'https://helloworld-48dff.firebaseio.com';
+  const DEFAULT_TOKEN = 'AIzaSyAuz1ICCDhvX70gjOJPoPo0fMZfiav2fqc';
+  const DEFAULT_PATH = '/firebase/line';
 
-// --- storing a string ---
-$firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
+  $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
-// --- reading the stored string ---
-$name = $firebase->get(DEFAULT_PATH . '/name/contact001');
+  // --- storing an array ---
+  $test = array(
+      "foo" => "bar",
+      "i_love" => "lamp",
+      "id" => 42
+  );
+  $dateTime = new DateTime();
+  $firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+
+  // --- storing a string ---
+  $firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
+
+  // --- reading the stored string ---
+  $name = $firebase->get(DEFAULT_PATH . '/name/contact001');
