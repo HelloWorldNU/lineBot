148bfe4e653ed2f0d83fc571becdc53dde4101b2
Ligament <ntrances@hotmail.com>
2017-03-11 16:33:12 +0700

    Update firebasetoline.php

--- firebasetoline.php
+++ firebasetoline.php
@@ -1,6 +1,5 @@
 <?php
 
-include ('vendor/autoload.php');
 include ('verdor/firebase-php/src/Firebase.php');
 
 $firebase = Firebase::fromServiceAccount('helloworld-48dff-firebase-adminsdk-zpze8-0bbee8d5ef.json');
