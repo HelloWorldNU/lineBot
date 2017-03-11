cdecb87d31398f9f81fe66944d81349553fb4137
Ligament <ntrances@hotmail.com>
2017-03-11 17:08:57 +0700

    Update firebasetoline.php

--- firebasetoline.php
+++ firebasetoline.php
@@ -7,18 +7,35 @@ $databaseUri = 'https://helloworld-48dff.firebaseio.com';
 
 $firebase = Firebase::fromDatabaseUriAndSecret($databaseUri, $secret);
 
-$database = $firebase->getDatabase();
+//$database = $firebase->getDatabase();
 
-$newPost = $database
-    ->getReference('blog/posts')
-    ->push([
-        'title' => 'Post title',
-        'body' => 'This should probably be longer.'
-    ]);
+//$newPost = $database
+//    ->getReference('blog/posts')
+//    ->push([
+//        'title' => 'Post title',
+//        'body' => 'This should probably be longer.'
+//    ]);
 
-$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
-$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+//$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+//$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
 
-$newPost->getChild('title')->set('Changed post title');
-$newPost->getValue(); // Fetches the data from the realtime database
-$newPost->remove();
+//$newPost->getChild('title')->set('Changed post title');
+//$newPost->getValue(); // Fetches the data from the realtime database
+//$newPost->remove();
+
+$tokenHandler = $firebase->getTokenHandler();
+
+$uid = 'a-uid';
+$claims = ['foo' => 'bar']; // optional
+
+// Returns a Lcobucci\JWT\Token instance
+$customToken = $tokenHandler->createCustomToken($uid, $claims);
+echo $token; // "eyJ0eXAiOiJKV1..."
+
+$idTokenString = 'eyJhbGciOiJSUzI1...';
+// Returns a Lcobucci\JWT\Token instance
+$idToken = $tokenHandler->verifyIdToken($idTokenString);
+
+$uid = $idToken->getClaim('sub');
+
+echo $uid; // 'a-uid'
